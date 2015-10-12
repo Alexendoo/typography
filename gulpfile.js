e2c2159a9c3e6917b09ac1ff3747e0786c2b38c7
Alexendoo <alex@macleod.io>
2015-10-12 20:14:25 +0100

    Tabs to spaces

--- gulpfile.js
+++ gulpfile.js
@@ -6,36 +6,36 @@ var path        = require('path');
 var browserSync = require('browser-sync');
 
 var src = {
-	scss: './app/scss/*.scss',
-	html: './app/html/',
-	jade: './app/jade/',
-	json: './app/json/'
+    scss: './app/scss/*.scss',
+    html: './app/html/',
+    jade: './app/jade/',
+    json: './app/json/'
 };
 
 gulp.task('serve', function() {
-	browserSync({
-		server: "./build"
-	});
+    browserSync({
+        server: "./build"
+    });
 
-	gulp.watch(src.scss, ['sass']);
-	gulp.watch([src.jade + '**/*.jade', src.json + '*.json'], ['templates']);
+    gulp.watch(src.scss, ['sass']);
+    gulp.watch([src.jade + '**/*.jade', src.json + '*.json'], ['templates']);
 });
 
 gulp.task('templates', function() {
-	return gulp.src(src.jade + '*.jade')
-		.pipe(data(function(file) {
-			return require(src.json + path.basename(file.path, '.jade') + '.json');
-		}))
-		.pipe(jade())
-		.pipe(gulp.dest('build'))
-		.on("end", browserSync.reload);
+    return gulp.src(src.jade + '*.jade')
+        .pipe(data(function(file) {
+            return require(src.json + path.basename(file.path, '.jade') + '.json');
+        }))
+        .pipe(jade())
+        .pipe(gulp.dest('build'))
+        .on("end", browserSync.reload);
 });
 
 gulp.task('sass', function() {
-	return gulp.src(src.scss)
-		.pipe(sass().on('error', sass.logError))
-		.pipe(gulp.dest('build/css'))
-		.pipe(browserSync.reload({stream: true}));
+    return gulp.src(src.scss)
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('build/css'))
+        .pipe(browserSync.reload({stream: true}));
 });
 
 gulp.task('default', ['serve']);
